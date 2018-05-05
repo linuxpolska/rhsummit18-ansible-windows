9f74c6f62c78176b23df7125d1d70204d74e02c6
tdziedzic <tomasz.dziedzic@linuxpolska.pl>
2018-05-05 10:22:39 +0200

    add secrets.yml and Vault password option for deploy.sh.

--- lab01-azure_provisioning/deploy.sh
+++ lab01-azure_provisioning/deploy.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i inventory -e @../azure.yml -e ansible_python_interpreter=$(which python) site.yml
+ansible-playbook --ask-vault-pass -i inventory -e @../secrets.yml -e @../azure.yml -e ansible_python_interpreter=$(which python) site.yml
