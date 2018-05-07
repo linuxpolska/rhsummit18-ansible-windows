2636b3f29d32bd15d4b1c68625c7493d3d719ba4
konrad rzentarzewski <konrad.rzentarzewski@linuxpolska.pl>
2018-05-07 19:44:16 +0200

    simple troubleshooting lab

---
+++ lab03-troubleshoot/setup.sh
@@ -0,0 +1,3 @@
+ansible-playbook -i ../instances.txt site.yml
+# ansible-playbook -i ../instances.txt site.yml -vvvvv
+# ANSIBLE_KEEP_REMOTE_FILES=1 ansible-playbook -i ../instances.txt site.yml -vvvvv
